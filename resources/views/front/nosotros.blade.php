3aa7ae2feb31aef438450de04be69c001eb81896
Michael <mikeed1998@gmail.com>
2024-06-24 16:55:11 -0600

    nosotros, avance en la portada

--- resources/views/front/nosotros.blade.php
+++ resources/views/front/nosotros.blade.php
@@ -3,13 +3,51 @@
 @section('title', 'Nosotros')
 
 @section('extracss')
-   
+    <style>
+        .fondo-nosotros {
+            height: 820px;
+            background-color: #0287DA;
+        }
+    </style>
 @endsection
 
 @section('content')
 
     
-
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <div class="row">
+                    <div class="col position-relative fondo-nosotros">
+                        <div class="col-12 position-absolute border top-0 start-0">
+                            <div class="row mt-5">
+                                <div class="col-lg-11 col-md-9 col-12 mx-auto mt-5 position-relative">
+                                    <div class="row">
+                                        <div class="col-7">
+                                            <img src="{{ asset('img/photos/nosotros/ragnar.png') }}" alt="" class="img-fluid">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="col-12 position-absolute border bottom-0 end-0">
+                            <div class="row mt-5">
+                                <div class="col-lg-11 col-md-9 col-12 mx-auto mt-5 position-relative text-end">
+                                    <div class="row">
+                                        <div class="col-5"></div>
+                                        <div class="col-7">
+                                            <img src="{{ asset('img/photos/nosotros/ragnar.png') }}" alt="" class="img-fluid">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     
 
 @endsection
